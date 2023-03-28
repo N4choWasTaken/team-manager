ee17a9e3e64fd0afd40c9e55edd4631aab0d594c
N4choWasTaken <thierrygmuer@outlook.com>
2023-03-28 16:48:30 +0200

    :sparkles: route / sends the status Code

--- src/routes.ts
+++ src/routes.ts
@@ -3,7 +3,7 @@ import { Router } from 'express';
 const routes = Router();
 
 routes.get('/', (req, res) => {
-  return res.json({ message: 'Hello World' });
+  return res.json({ message: 'Hello World' }).status(200);
 });
 
 export default routes;
--- tests/unit/api.test.ts
+++ tests/unit/api.test.ts
@@ -1,17 +1,18 @@
 import {request} from "./helpers"
 
 interface iResult {
-    message: String
+    message: String,
+    status: Number
 } 
 
 describe("Get /", () => {
-    it("Should get Hello World",async () => {
+    it("Should get Hello World with status code 200",async () => {
         // Given
         const sut = await (await request.get('/'))
-        const expected: iResult = {message: "Hello World"}
+        const expected: iResult = {message: "Hello World", status: 200}
         
         // When
-        const actual: iResult = sut.body
+        const actual: iResult = {message: sut.body.message, status: sut.statusCode}
 
         //Then
         expect(actual).toStrictEqual(expected);
