2af1c9daef383e26c83adffaa9ee9d373cef8408
N4choWasTaken <thierrygmuer@outlook.com>
2023-03-31 16:05:56 +0200

    :recycle:(userRepository.ts): Db call is moved to repository

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,11 +1,10 @@
+import { UserRepository } from "../repositories/UserRepository";
 import { Request, Response } from 'express';
-import { PrismaClient } from '@prisma/client';
-
-const prisma = new PrismaClient();
 
 const getAllUsers = async (req: Request, res: Response) => {
-	const users = await prisma.user.findMany()
-	res.json(users)
+	const userRepository = new UserRepository()
+
+	userRepository.getUsers(req, res)
 }
 
 export default { getAllUsers };
