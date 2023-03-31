c364e00f53575c9b2f8a2d88766816d8e8b4aa01
N4choWasTaken <thierrygmuer@outlook.com>
2023-03-31 14:07:00 +0200

    :recycle:(userController.ts): getAllUsers is moved to userController

--- src/routes.ts
+++ src/routes.ts
@@ -1,16 +1,11 @@
 import { Router } from 'express';
 import helloController from './controllers/helloController';
-import { PrismaClient } from '@prisma/client';
-
-const prisma = new PrismaClient();
+import userController from './controllers/userController'
 
 const routes = Router();
 
 routes.get('/', helloController.getHelloMessage);
 
-routes.get('/users', async (req, res) => {
-	const users = await prisma.user.findMany()
-	res.json(users)
-  })
+routes.get('/users', userController.getAllUsers)
 
 export default routes;
