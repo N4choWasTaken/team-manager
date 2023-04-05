61b172d09a6eca8fea57b6c3b2d57a82e8b38629
N4choWasTaken <thierrygmuer@outlook.com>
2023-04-05 14:57:47 +0200

    :technologist:IOC(container.ts) iUserRepository is handled by IOC

--- src/repositories/UserRepository.ts
+++ src/repositories/UserRepository.ts
@@ -1,11 +1,17 @@
 import { Request, Response } from 'express';
 import { PrismaClient } from '@prisma/client';
+import { injectable } from 'inversify';
 
-export class UserRepository {
+@injectable()
+export class UserRepository implements iUserRepository {
 	prisma = new PrismaClient();
 
-	async getUsers(req: Request, res: Response) {
+	async getUsers(req: Request, res: Response): Promise<void> {
 		const users = await this.prisma.user.findMany()
 		res.json(users)
 	}
+}
+
+export interface iUserRepository {
+	getUsers(req: Request, res: Response): Promise<void>
 }
--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,15 @@
+import 'reflect-metadata';
 import { Router } from 'express';
 import helloController from './controllers/helloController';
-import userController from './controllers/userController'
+import UserController from './controllers/userController';
+import { UserRepository } from './repositories/UserRepository';
+import { container } from './container';
 
 const routes = Router();
+const userController = new UserController(container.get<UserRepository>(UserRepository));
 
 routes.get('/', helloController.getHelloMessage);
 
-routes.get('/users', userController.getAllUsers)
+routes.get('/users', userController.getAllUsers);
 
 export default routes;
