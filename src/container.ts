a81065a237fe3631dc3e364626d38a4504722f36
N4choWasTaken <thierrygmuer@outlook.com>
2023-04-05 15:06:32 +0200

    :technologist:IOC(userController.ts) iUserController is handled by IOC

--- src/container.ts
+++ src/container.ts
@@ -1,8 +1,10 @@
 import { Container } from "inversify";
 import { UserRepository, iUserRepository } from "./repositories/UserRepository";
+import UserController, { iUserController } from "./controllers/userController";
 
 const container = new Container();
 
 container.bind<iUserRepository>(UserRepository).to(UserRepository);
+container.bind<iUserController>(UserController).to(UserController);
 
 export { container }
--- src/routes.ts
+++ src/routes.ts
@@ -2,11 +2,10 @@ import 'reflect-metadata';
 import { Router } from 'express';
 import helloController from './controllers/helloController';
 import UserController from './controllers/userController';
-import { UserRepository } from './repositories/UserRepository';
 import { container } from './container';
 
 const routes = Router();
-const userController = new UserController(container.get<UserRepository>(UserRepository));
+const userController = container.get<UserController>(UserController);
 
 routes.get('/', helloController.getHelloMessage);
 
